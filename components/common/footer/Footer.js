d5a51d9dbed2db87e7dfd80a7fbe18b345992b6e
lucian-dev <lucian.dev13@gmail.com>
2021-12-29 21:50:37 +0200

    #footer

--- components/common/footer/Footer.js
+++ components/common/footer/Footer.js
@@ -1,12 +1,20 @@
-import styles from "./Footer.module.scss";
+import styles from './Footer.module.scss';
 
 const Footer = () => {
   return (
     <footer className={styles.mainFooter}>
-      <div className="copyright">
-        <span>
-          {new Date().getFullYear()} &copy; Lucian-DEV. All Rights Reserved
-        </span>
+      <div className={styles.stack}>
+        Built with{' '}
+        <a href="https://nextjs.org" target="_blank" rel="noreferrer noopener">
+          NextJS
+        </a>
+        . Data from WordPress. Hosted on{' '}
+        <a href="https://netlify.com" target="_blank" rel="noreferrer noopener">
+          Netlify
+        </a>
+      </div>
+      <div className={styles.copyright}>
+        <span>{new Date().getFullYear()} &copy; Lucian-DEV</span>
       </div>
     </footer>
   );
